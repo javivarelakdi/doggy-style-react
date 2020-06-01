b2b16863dd5359a02cea707290476d1b1ba5c49b
Javier Varela <jvarela@strands.com>
2020-06-01 03:31:04 +0200

    validation form

--- src/views/EventsView.js
+++ src/views/EventsView.js
@@ -23,7 +23,13 @@ export default class EventsView extends Component {
     isLoading:true,
     errorStatus: "",
     lng:"",
-    lat:""
+    lat:"",
+    nameValid: false,
+    descriptionValid: false,
+    dateValid: false,
+    initValid: false,
+    endValid: false,
+    formErrors: {name: "", description: "", date: "", init: "", end: ""}
   }
 
   componentDidMount() {
@@ -85,9 +91,12 @@ export default class EventsView extends Component {
   };
 
   handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    const fieldName = e.target.name;
+    const value = e.target.value;
+    this.setState(
+      {[e.target.name]: e.target.value},
+      () => { this.validateField(fieldName, value) }
+    );
   };
 
   handleLocation = (lng, lat) =>{
@@ -101,9 +110,52 @@ export default class EventsView extends Component {
     this.props.history.push(`/events/${eventId}`, { from: this.props.location })
   }
 
+  validateField = (fieldName, value) => {
+    let {formErrors, nameValid, descriptionValid, dateValid, initValid, endValid } = this.state
+    
+  
+    switch(fieldName) {
+      case 'name':
+        nameValid = value.length > 6 && value.length < 20; 
+        formErrors.name = nameValid ? '' : 'description is lower than 6 characters or higher than 20 characters';
+        break;
+      case 'description':
+        descriptionValid = value.length > 6 && value.length < 120;
+        formErrors.description = descriptionValid ? '': 'description is lower than 6 characters or higher than 120 characters';
+        break;
+      case 'date':
+        dateValid = new Date(value).getTime() > new Date().getTime()
+        formErrors.date = dateValid ? '': 'date must be a day in the future';
+        break;
+      case 'initTime':
+        initValid = value.match(/\d+:\d+/);
+        formErrors.init = initValid ? '': 'enter a valid time format';
+        break;
+      case 'endTime':
+        endValid = value.match(/\d+:\d+/);
+        formErrors.end = endValid ? '': 'enter a valid time format';
+        break;
+      default:
+        break;
+    }
+    this.setState({
+      formErrors,
+      nameValid,
+      descriptionValid,
+      dateValid,
+      initValid,
+      endValid
+    }, this.validateForm);
+  }
+
+  validateForm = () => {
+    let { nameValid, descriptionValid, dateValid, initValid, endValid } = this.state
+    this.setState({formValid: nameValid && descriptionValid && dateValid && initValid && endValid});
+  }
+
   render() {
 
-    const { screen, events, isLoading, errorStatus} = this.state;
+    const { screen, events, isLoading, errorStatus, formErrors } = this.state;
     return (
       
       <div className="App__container">
@@ -162,10 +214,12 @@ export default class EventsView extends Component {
                 />
             </div>
             <Form 
-              onSubmit={this.handleSubmit}>
+              onSubmit={this.handleSubmit}
+              disabled={!this.state.formValid}>
               <Field
                 label="name"
                 type="text"
+                validationError={formErrors.name}
                 name="name"
                 onChange={this.handleChange}
                 />
@@ -173,24 +227,28 @@ export default class EventsView extends Component {
                 label="description"
                 type="text"
                 name="description"
+                validationError={formErrors.description}
                 onChange={this.handleChange}
                 />
               <Field             
                 label="date"
                 type="date"
                 name="date"
+                validationError={formErrors.date}
                 onChange={this.handleChange}
                 />
               <Field 
                 label="initial time"
                 type="time"
                 name="initTime"
+                validationError={formErrors.init}
                 onChange={this.handleChange}
                 />
               <Field
                 label="end time"
                 type="time"
                 name="endTime"
+                validationError={formErrors.end}
                 onChange={this.handleChange}
                 />
             </Form>
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -22,6 +22,8 @@ export default class Signup extends Component {
     aboutValid: false,
     birthValid: false,
     formValid: false,
+    breedValid: false,
+    genderValid: false,
     formErrors: {username: "", password: "", about: "", birth: ""}
   };
 
@@ -83,26 +85,33 @@ export default class Signup extends Component {
   };
 
   validateField = (fieldName, value) => {
-    let {formErrors, usernameValid, passwordValid, birthValid, aboutValid } = this.state
+    let {formErrors, usernameValid, passwordValid, birthValid, aboutValid, genderValid, breedValid } = this.state
     
-    //const today = new Date();
   
     switch(fieldName) {
       case 'username':
-        usernameValid = value.match(/^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/i); 
-        formErrors.username = usernameValid ? '' : 'username is not 8 to 20 characters or has spaces or strange characters';
+        usernameValid = value.match(/^(?=[a-zA-Z0-9._]{6,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/i); 
+        formErrors.username = usernameValid ? '' : 'username is not 6 to 20 characters or has spaces or strange characters';
         break;
       case 'password':
         passwordValid = value.length > 6;
         formErrors.password = passwordValid ? '': 'password is lower than 6 characters';
         break;
       case 'birth':
-        birthValid = true
+        birthValid = new Date(value).getTime() < new Date().getTime()
         formErrors.birth = birthValid ? '': 'birth date must be a day in the past';
         break;
       case 'about':
         aboutValid = value.length > 6 && value.length < 120;
         formErrors.about = aboutValid ? '': 'about field is lower than 6 or higher than 120 characters';
+        break;
+      case 'breed':
+        breedValid = value.length > 0;
+        formErrors.breed = breedValid ? '': 'breed must be selected';
+        break;
+      case 'gender':
+        genderValid = value.length > 0;
+        formErrors.gender = genderValid ? '': 'gender must be selected';
         break;
       default:
         break;
@@ -112,13 +121,15 @@ export default class Signup extends Component {
       usernameValid,
       passwordValid,
       birthValid,
-      aboutValid
+      aboutValid,
+      genderValid,
+      breedValid
     }, this.validateForm);
   }
 
   validateForm = () => {
-    let { usernameValid, passwordValid, birthValid, aboutValid } = this.state
-    this.setState({formValid: usernameValid && passwordValid && birthValid && aboutValid});
+    let { usernameValid, passwordValid, birthValid, aboutValid, breedValid, genderValid } = this.state
+    this.setState({formValid: usernameValid && passwordValid && birthValid && aboutValid && breedValid && genderValid});
   }
 
   render() {
@@ -173,7 +184,7 @@ export default class Signup extends Component {
             type="select"
             name="breed"
             value={breed}
-            options={this.state.breedOptions}
+            options={[{value: "", text:"choose breed"}, ...this.state.breedOptions]}
             onChange={this.handleChange}
             />
           <Field
@@ -182,7 +193,7 @@ export default class Signup extends Component {
             type="select"
             name="gender"
             value={gender}
-            options={[{value: "male", text:"male"},{value: "female", text:"female"},{value: "non-binary", text:"non binary"}]}
+            options={[{value: "", text:"choose gender"}, {value: "male", text:"male"},{value: "female", text:"female"},{value: "non-binary", text:"non binary"}]}
             onChange={this.handleChange}
             />
         </Form>
